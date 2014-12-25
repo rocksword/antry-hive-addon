04121b6dddd7e9f59ca16a82a82aaf85f5a23365
rocksword <rocksword@163.com>
2014-12-25 19:28:40 +0800

    UDF

--- src/main/java/com/an/antry/hive/udf/UDFZodiacSign.java
+++ src/main/java/com/an/antry/hive/udf/UDFZodiacSign.java
@@ -1,5 +1,45 @@
 package com.an.antry.hive.udf;
 
-public class UDFZodiacSign {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class UDFZodiacSign extends UDF {
+    private SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+
+    public UDFZodiacSign() {
+    }
+
+    public String evaluate(Date bday) {
+        return this.evaluate(bday.getMonth(), bday.getDay());
+    }
+
+    public String evaluate(String bday) {
+        Date date = null;
+        try {
+            date = df.parse(bday);
+        } catch (Exception ex) {
+            return null;
+        }
+        return this.evaluate(date.getMonth() + 1, date.getDay());
+    }
+
+    public String evaluate(Integer month, Integer day) {
+        if (month == 1) {
+            if (day < 20) {
+                return "Capricorn";
+            } else {
+                return "Aquarius";
+            }
+        }
+        if (month == 2) {
+            if (day < 19) {
+                return "Aquarius";
+            } else {
+                return "Pisces";
+            }
+        }
+        return null;
+    }
 }
